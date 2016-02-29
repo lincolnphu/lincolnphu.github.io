80b51cfe7d6abfe1fb5c8a57246fe0bf627d888b
lincolnphu <fjfoursg@gmail.com>
2016-02-29 13:00:15 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
+'use strict';
 
 var UserTrackBox = React.createClass({
   displayName: 'UserTrackBox',
 
+  getInitialState: function getInitialState() {
+    return {
+      data: [],
+      limitNumber: '',
+      album: '',
+      image: '',
+      url: '',
+      name: '',
+      artist: ''
+    };
+  },
   loadMusicFromServer: function loadMusicFromServer() {
-    var user = 'lincolnphu';
-    var api = '6510c6b46fd1c71571bc40ee7037e1a9';
-    var limitNumber = 49;
-    var windowSize = window.innerWidth;
-
+    var user = 'lincolnphu',
+        api = '6510c6b46fd1c71571bc40ee7037e1a9',
+        limitNumber = 49,
+        windowSize = window.innerWidth;
     if (windowSize > 1465) {
       limitNumber = 98;
     } else if (windowSize == 1440) {
@@ -21,14 +32,13 @@ var UserTrackBox = React.createClass({
     fetch(url).then(function (response) {
       return response.json();
     }).then((function (json) {
-      var data = json.recenttracks.track;
-      var listening = data[0];
-      var image = listening.image[3]["#text"];
-      var album = listening.album["#text"];
-      var name = listening.name;
-      var artist = listening.artist["#text"];
-      console.log(listening);
-      var url = listening.url;
+      var data = json.recenttracks.track,
+          listening = data[0],
+          image = listening.image[3]["#text"],
+          album = listening.album["#text"],
+          name = listening.name,
+          artist = listening.artist["#text"],
+          url = listening.url;
       if (this.isMounted()) {
         this.setState({
           data: data,
@@ -38,31 +48,15 @@ var UserTrackBox = React.createClass({
           url: url,
           name: name,
           artist: artist
-
         });
       }
     }).bind(this))['catch'](function (err) {
       console.log('parsing failed', err);
     });
   },
-  getInitialState: function getInitialState() {
-    return {
-      data: [],
-      limitNumber: '',
-      album: '',
-      image: '',
-      url: '',
-      name: '',
-      artist: ''
-    };
-  },
   componentDidMount: function componentDidMount() {
     this.loadMusicFromServer();
     setInterval(this.loadMusicFromServer, 60000);
-  },
-
-  shouldComponentUpdate: function shouldComponentUpdate() {
-    return true;
   },
   handleChange: function handleChange(i) {
     this.setState({
@@ -74,32 +68,39 @@ var UserTrackBox = React.createClass({
     });
   },
   offLine: function offLine() {
-
     return React.createElement(
       'div',
       { className: 'LastFmlist' },
-      React.createElement(TrackList, { onClick: this.handleChange, data: this.state.data }),
-      React.createElement(ListenTrack, { image: this.state.image, name: this.state.name, artist: this.state.artist, url: this.state.url, album: this.state.album }),
-      React.createElement(
-        'h2',
-        null,
-        '不好意识，我不在哦'
-      )
+      React.createElement(TrackList, {
+        onClick: this.handleChange,
+        data: this.state.data }),
+      React.createElement(ListenTrack, { image: this.state.image,
+        name: this.state.name,
+        artist: this.state.artist,
+        url: this.state.url,
+        album: this.state.album })
     );
   },
   Online: function Online() {
-    var musicarray = this.state.data;
+    var tipsDiv = React.createElement(
+      'div',
+      { ref: 'tips', id: 'tt1', className: 'icon material-icons' },
+      'add'
+    ),
+        musicarray = this.state.data;
     musicarray.splice(-1, 1);
     return React.createElement(
       'div',
       { className: 'LastFmlist' },
-      React.createElement(TrackList, { onClick: this.handleChange, data: musicarray }),
-      React.createElement(ListenTrack, { image: this.state.image, name: this.state.name, artist: this.state.artist, url: this.state.url, album: this.state.album }),
-      React.createElement(
-        'h2',
-        null,
-        '我现在，在线哦！'
-      )
+      React.createElement(TrackList, {
+        onClick: this.handleChange,
+        data: musicarray }),
+      React.createElement(ListenTrack, {
+        image: this.state.image,
+        name: this.state.name,
+        artist: this.state.artist,
+        url: this.state.url,
+        album: this.state.album })
     );
   },
   render: function render() {
@@ -122,19 +123,19 @@ var ListenTrack = React.createClass({
       var newimg = 'http://img2-ak.lst.fm/i/u/174s/e04ce91798e34c36b21a85a9fab01b40.jpg';
       this.props.image = newimg;
     }
-    var imgUrl = this.props.image;
-    var styles = {
+    var imgUrl = this.props.image,
+        styles = {
       backgroundImage: 'url(' + imgUrl + ')',
       width: '300',
       height: '300'
     };
-
     return React.createElement(
       'div',
       null,
       React.createElement(
         'div',
-        { style: styles, className: 'demo-card-image mdl-card mdl-shadow--2dp' },
+        { style: styles,
+          className: 'demo-card-image  mdl-card mdl-shadow--2dp' },
         React.createElement('div', { className: 'mdl-card__title mdl-card--expand' }),
         React.createElement(
           'div',
@@ -162,19 +163,23 @@ var ListenTrack = React.createClass({
     );
   }
 });
-var styles = {
-  background: 'url(http://i54.tinypic.com/4zuxif.jpg)'
-};
 
 function TrackList(props) {
+  var divStyle = {
+    float: 'right',
+    position: 'relative',
+    margin: '0',
+    bottom: '0',
+    width: '448px'
+  };
   return React.createElement(
     'div',
     { style: divStyle, className: 'userTrackBox' },
     props.data.map(function (result, i) {
-      var _furl = result.image[1]["#text"];
-      var _fartist = result.artist["#text"];
-      var _fname = result.name;
-      var _fhref = result.url;
+      var _furl = result.image[1]["#text"],
+          _fartist = result.artist["#text"],
+          _fname = result.name,
+          _fhref = result.url;
 
       if (_furl === "") {
         var newimg = 'http://img2-ak.lst.fm/i/u/174s/e04ce91798e34c36b21a85a9fab01b40.jpg';
@@ -194,20 +199,13 @@ var Track = React.createClass({
   displayName: 'Track',
 
   render: function render() {
+    var imgStyle = {
+      height: '64px',
+      width: '64px'
+    };
     return React.createElement('img', { style: imgStyle, onMouseOver: this.props.onClick, src: this.props.furl });
   }
 });
-var imgStyle = {
-  height: '64px',
-  width: '64px'
-};
-var divStyle = {
-  float: 'right',
-  position: 'relative',
-  margin: '0',
-  bottom: '0',
-  width: '448px'
-};
 
 
 

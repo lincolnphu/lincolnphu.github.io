c2895912efc07ca059ff3a5ec40182a809ad285d
lincolnphu <fjfoursg@gmail.com>
2016-04-07 07:46:24 +0800

    Create tops.js

---
+++ src/tops.js
@@ -0,0 +1,383 @@
+var tops = {
+  "toptracks": {
+    "track": [
+      {
+        "name": "U Can't Touch This",
+        "duration": "254",
+        "playcount": "267",
+        "mbid": "edbdeaf2-51e4-4b51-bcb6-174a160b8899",
+        "url": "http://www.last.fm/music/MC+Hammer/_/U+Can%27t+Touch+This",
+        "streamable": {
+          "#text": "0",
+          "fulltrack": "0"
+        },
+        "artist": {
+          "name": "MC Hammer",
+          "mbid": "cba94150-9113-4017-acc2-a8cc0313a084",
+          "url": "http://www.last.fm/music/MC+Hammer"
+        },
+        "image": [
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/34s/a12348dca8c747d694c1175ec5d9c9fc.png",
+            "size": "small"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/64s/a12348dca8c747d694c1175ec5d9c9fc.png",
+            "size": "medium"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/174s/a12348dca8c747d694c1175ec5d9c9fc.png",
+            "size": "large"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/300x300/a12348dca8c747d694c1175ec5d9c9fc.png",
+            "size": "extralarge"
+          }
+        ],
+        "@attr": {
+          "rank": "1"
+        }
+      },
+      {
+        "name": "Dosu-Koi (E. Honda Stage)",
+        "duration": "123",
+        "playcount": "236",
+        "mbid": "6e977852-3712-4fd7-a6a0-b815217433eb",
+        "url": "http://www.last.fm/music/djpretzel/_/Dosu-Koi+(E.+Honda+Stage)",
+        "streamable": {
+          "#text": "0",
+          "fulltrack": "0"
+        },
+        "artist": {
+          "name": "djpretzel",
+          "mbid": "3477143a-7c28-42d9-be41-cb641ced3a1a",
+          "url": "http://www.last.fm/music/djpretzel"
+        },
+        "image": [
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/34s/070b1f021279439bb1da9dd29b3deefb.png",
+            "size": "small"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/64s/070b1f021279439bb1da9dd29b3deefb.png",
+            "size": "medium"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/174s/070b1f021279439bb1da9dd29b3deefb.png",
+            "size": "large"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/300x300/070b1f021279439bb1da9dd29b3deefb.png",
+            "size": "extralarge"
+          }
+        ],
+        "@attr": {
+          "rank": "2"
+        }
+      },
+      {
+        "name": "Art of Xen",
+        "duration": "191",
+        "playcount": "228",
+        "mbid": "",
+        "url": "http://www.last.fm/music/APM+Music/_/Art+of+Xen",
+        "streamable": {
+          "#text": "0",
+          "fulltrack": "0"
+        },
+        "artist": {
+          "name": "APM Music",
+          "mbid": "",
+          "url": "http://www.last.fm/music/APM+Music"
+        },
+        "image": [
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/34s/c3e9220e72b84a8dad233e936fb2ffeb.png",
+            "size": "small"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/64s/c3e9220e72b84a8dad233e936fb2ffeb.png",
+            "size": "medium"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/174s/c3e9220e72b84a8dad233e936fb2ffeb.png",
+            "size": "large"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/300x300/c3e9220e72b84a8dad233e936fb2ffeb.png",
+            "size": "extralarge"
+          }
+        ],
+        "@attr": {
+          "rank": "3"
+        }
+      },
+      {
+        "name": "はるかな旅 (TVオープニングテーマ)",
+        "duration": "0",
+        "playcount": "171",
+        "mbid": "",
+        "url": "http://www.last.fm/music/%E5%90%89%E7%94%B0%E6%BD%94/_/%E3%81%AF%E3%82%8B%E3%81%8B%E3%81%AA%E6%97%85%C2%A0(TV%E3%82%AA%E3%83%BC%E3%83%97%E3%83%8B%E3%83%B3%E3%82%B0%E3%83%86%E3%83%BC%E3%83%9E)",
+        "streamable": {
+          "#text": "0",
+          "fulltrack": "0"
+        },
+        "artist": {
+          "name": "吉田潔",
+          "mbid": "ad46bbda-c1a4-4e2e-8459-b34a1a441a9b",
+          "url": "http://www.last.fm/music/%E5%90%89%E7%94%B0%E6%BD%94"
+        },
+        "image": [
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/34s/5662bee443e347f2babd855e876233ad.png",
+            "size": "small"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/64s/5662bee443e347f2babd855e876233ad.png",
+            "size": "medium"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/174s/5662bee443e347f2babd855e876233ad.png",
+            "size": "large"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/300x300/5662bee443e347f2babd855e876233ad.png",
+            "size": "extralarge"
+          }
+        ],
+        "@attr": {
+          "rank": "4"
+        }
+      },
+      {
+        "name": "“エトピリカ”",
+        "duration": "0",
+        "playcount": "166",
+        "mbid": "",
+        "url": "http://www.last.fm/music/%E8%91%89%E5%8A%A0%E7%80%AC%E5%A4%AA%E9%83%8E/_/%E2%80%9C%E3%82%A8%E3%83%88%E3%83%94%E3%83%AA%E3%82%AB%E2%80%9D",
+        "streamable": {
+          "#text": "0",
+          "fulltrack": "0"
+        },
+        "artist": {
+          "name": "葉加瀬太郎",
+          "mbid": "784e4c0f-0ed8-46eb-b780-21b236c14212",
+          "url": "http://www.last.fm/music/%E8%91%89%E5%8A%A0%E7%80%AC%E5%A4%AA%E9%83%8E"
+        },
+        "image": [
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/34s/058fdc111a2d4c7dbf9f0db168425822.png",
+            "size": "small"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/64s/058fdc111a2d4c7dbf9f0db168425822.png",
+            "size": "medium"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/174s/058fdc111a2d4c7dbf9f0db168425822.png",
+            "size": "large"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/300x300/058fdc111a2d4c7dbf9f0db168425822.png",
+            "size": "extralarge"
+          }
+        ],
+        "@attr": {
+          "rank": "5"
+        }
+      },
+      {
+        "name": "はるかな旅 (TVオープニングテーマ)",
+        "duration": "0",
+        "playcount": "166",
+        "mbid": "",
+        "url": "http://www.last.fm/music/%E5%90%89%E7%94%B0%E6%BD%94/_/%E3%81%AF%E3%82%8B%E3%81%8B%E3%81%AA%E6%97%85+(TV%E3%82%AA%E3%83%BC%E3%83%97%E3%83%8B%E3%83%B3%E3%82%B0%E3%83%86%E3%83%BC%E3%83%9E)",
+        "streamable": {
+          "#text": "0",
+          "fulltrack": "0"
+        },
+        "artist": {
+          "name": "吉田潔",
+          "mbid": "ad46bbda-c1a4-4e2e-8459-b34a1a441a9b",
+          "url": "http://www.last.fm/music/%E5%90%89%E7%94%B0%E6%BD%94"
+        },
+        "image": [
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/34s/5662bee443e347f2babd855e876233ad.png",
+            "size": "small"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/64s/5662bee443e347f2babd855e876233ad.png",
+            "size": "medium"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/174s/5662bee443e347f2babd855e876233ad.png",
+            "size": "large"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/300x300/5662bee443e347f2babd855e876233ad.png",
+            "size": "extralarge"
+          }
+        ],
+        "@attr": {
+          "rank": "6"
+        }
+      },
+      {
+        "name": "獨領風騷",
+        "duration": "0",
+        "playcount": "148",
+        "mbid": "",
+        "url": "http://www.last.fm/music/%E6%BC%94%E5%A5%8F%E6%9B%B2/_/%E7%8D%A8%E9%A0%98%E9%A2%A8%E9%A8%B7",
+        "streamable": {
+          "#text": "0",
+          "fulltrack": "0"
+        },
+        "artist": {
+          "name": "演奏曲",
+          "mbid": "",
+          "url": "http://www.last.fm/music/%E6%BC%94%E5%A5%8F%E6%9B%B2"
+        },
+        "image": [
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/34s/169d3c427d26443c9d606b0e3538ef40.png",
+            "size": "small"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/64s/169d3c427d26443c9d606b0e3538ef40.png",
+            "size": "medium"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/174s/169d3c427d26443c9d606b0e3538ef40.png",
+            "size": "large"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/300x300/169d3c427d26443c9d606b0e3538ef40.png",
+            "size": "extralarge"
+          }
+        ],
+        "@attr": {
+          "rank": "7"
+        }
+      },
+      {
+        "name": "少林雄风",
+        "duration": "0",
+        "playcount": "147",
+        "mbid": "",
+        "url": "http://www.last.fm/music/%E9%B2%8D%E6%AF%94%E8%BE%BE/_/%E5%B0%91%E6%9E%97%E9%9B%84%E9%A3%8E",
+        "streamable": {
+          "#text": "0",
+          "fulltrack": "0"
+        },
+        "artist": {
+          "name": "鲍比达",
+          "mbid": "",
+          "url": "http://www.last.fm/music/%E9%B2%8D%E6%AF%94%E8%BE%BE"
+        },
+        "image": [
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/34s/f08455697931410bb0d8b1b989b9ae2e.png",
+            "size": "small"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/64s/f08455697931410bb0d8b1b989b9ae2e.png",
+            "size": "medium"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/174s/f08455697931410bb0d8b1b989b9ae2e.png",
+            "size": "large"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/300x300/f08455697931410bb0d8b1b989b9ae2e.png",
+            "size": "extralarge"
+          }
+        ],
+        "@attr": {
+          "rank": "8"
+        }
+      },
+      {
+        "name": "Opening",
+        "duration": "0",
+        "playcount": "137",
+        "mbid": "",
+        "url": "http://www.last.fm/music/%E9%BB%84%E8%8B%B1%E5%8D%8E/_/Opening",
+        "streamable": {
+          "#text": "0",
+          "fulltrack": "0"
+        },
+        "artist": {
+          "name": "黄英华",
+          "mbid": "",
+          "url": "http://www.last.fm/music/%E9%BB%84%E8%8B%B1%E5%8D%8E"
+        },
+        "image": [
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/34s/9a552bed197744d59a4755bfbee49749.png",
+            "size": "small"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/64s/9a552bed197744d59a4755bfbee49749.png",
+            "size": "medium"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/174s/9a552bed197744d59a4755bfbee49749.png",
+            "size": "large"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/300x300/9a552bed197744d59a4755bfbee49749.png",
+            "size": "extralarge"
+          }
+        ],
+        "@attr": {
+          "rank": "9"
+        }
+      },
+      {
+        "name": "Harder Better Faster Stronger",
+        "duration": "177",
+        "playcount": "103",
+        "mbid": "",
+        "url": "http://www.last.fm/music/Mike+Tompkins/_/Harder+Better+Faster+Stronger",
+        "streamable": {
+          "#text": "0",
+          "fulltrack": "0"
+        },
+        "artist": {
+          "name": "Mike Tompkins",
+          "mbid": "2e831033-138d-4458-917e-c3e210c0f1ae",
+          "url": "http://www.last.fm/music/Mike+Tompkins"
+        },
+        "image": [
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/34s/36f64750d142427ab353d7a35b654761.png",
+            "size": "small"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/64s/36f64750d142427ab353d7a35b654761.png",
+            "size": "medium"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/174s/36f64750d142427ab353d7a35b654761.png",
+            "size": "large"
+          },
+          {
+            "#text": "http://img2-ak.lst.fm/i/u/300x300/36f64750d142427ab353d7a35b654761.png",
+            "size": "extralarge"
+          }
+        ],
+        "@attr": {
+          "rank": "10"
+        }
+      }
+    ],
+    "@attr": {
+      "user": "Lincolnphu",
+      "page": "1",
+      "perPage": "10",
+      "totalPages": "513",
+      "total": "5128"
+    }
+  }
+}

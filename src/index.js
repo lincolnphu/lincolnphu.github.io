3b22e5194054d5d1deeb58f056bffb48b53e80ba
lincolnphu <fjfoursg@gmail.com>
2016-04-18 22:48:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,90 +1,89 @@
 var FetchImg = React.createClass({
-  getInitialState: function() {
+  displayName: 'FetchImg',
+
+  getInitialState: function () {
     return {
-      name:'lincolnphu',
-      url:'https://ws.audioscrobbler.com/2.0/?method=',
-      api:'6510c6b46fd1c71571bc40ee7037e1a9',
-      fetchnumber:'20',
-      tracks:[],
-      page:1,
+      name: 'lincolnphu',
+      url: 'https://ws.audioscrobbler.com/2.0/?method=',
+      api: '6510c6b46fd1c71571bc40ee7037e1a9',
+      fetchnumber: '20',
+      tracks: [],
+      page: 1
     };
   },
-  componentDidMount: function() {
-    this.fetchData()
+  componentDidMount: function () {
+    this.fetchData();
     window.addEventListener('scroll', this.refresh);
   },
-  componentWillMount: function() {
-        window.removeEventListener('scroll', this.refresh);
+  componentWillMount: function () {
+    window.removeEventListener('scroll', this.refresh);
   },
-  fetchData:function(){
-    var {name,url,api,fetchnumber,page} = this.state;
-    var fetchUrl = url+'user.getrecenttracks'+'&user='+name+'&api_key='+api+'&format=json&limit='+fetchnumber
-    fetch(fetchUrl)
-      .then(function(response){
-        return response.json()
-      }).then(function(json){
-        var tracks = json.recenttracks.track
-        this.setState({
-          tracks:tracks
-        })
-      }.bind(this))
-  },
-  pushData:function(){
-
-  },
-  refresh:function(e){
-   if ((window.innerHeight + window.scrollY) == (document.body.offsetHeight)){
-  var {name,url,api,tracks,fetchnumber,page} = this.state;
-  var page = this.state.page+1
-  var fetchUrl = url+'user.getrecenttracks'+'&user='+name+'&api_key='+api+'&format=json&limit=3'+'&page='+page
-  fetch(fetchUrl)
-    .then(function(response){
-      return response.json()
-    }).then(function(json){
-      var newf = json.recenttracks.track
-      var newtracks = tracks.concat(newf)
+  fetchData: function () {
+    var { name, url, api, fetchnumber, page } = this.state;
+    var fetchUrl = url + 'user.getrecenttracks' + '&user=' + name + '&api_key=' + api + '&format=json&limit=' + fetchnumber;
+    fetch(fetchUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      var tracks = json.recenttracks.track;
       this.setState({
-        tracks:newtracks,
-        page:page
-      })
-    }.bind(this))
-}
+        tracks: tracks
+      });
+    }.bind(this));
   },
-  render:function(){
-    var {tracks} = this.state;
-    var topTrack = tracks
-    return(
-      <div className="grd">
-      <Info tracks={tracks}/>
-      <div onScroll={this.refresh}></div>
-      </div>
-    )
+  pushData: function () {},
+  refresh: function (e) {
+    if (window.innerHeight + window.scrollY == document.body.offsetHeight) {
+      var { name, url, api, tracks, fetchnumber, page } = this.state;
+      var page = this.state.page + 1;
+      var fetchUrl = url + 'user.getrecenttracks' + '&user=' + name + '&api_key=' + api + '&format=json&limit=3' + '&page=' + page;
+      fetch(fetchUrl).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        var newf = json.recenttracks.track;
+        var newtracks = tracks.concat(newf);
+        this.setState({
+          tracks: newtracks,
+          page: page
+        });
+      }.bind(this));
+    }
+  },
+  render: function () {
+    var { tracks } = this.state;
+    var topTrack = tracks;
+    return React.createElement(
+      'div',
+      { className: 'grd' },
+      React.createElement(Info, { tracks: tracks }),
+      React.createElement('div', { onScroll: this.refresh })
+    );
   }
-})
+});
 
 var Info = React.createClass({
-  render:function(){
-    var {tracks} = this.props;
-    var examples = tracks.map(function(d,i){
-      var name  = d.artist.name;
+  displayName: 'Info',
+
+  render: function () {
+    var { tracks } = this.props;
+    var examples = tracks.map(function (d, i) {
+      var name = d.artist.name;
       var image = d.image["1"]["#text"];
-      if (image === '' ){
-        image = 'http://cdns2.freepik.com/free-photo/_318-10795.jpg'
-
+      if (image === '') {
+        image = 'http://cdns2.freepik.com/free-photo/_318-10795.jpg';
       }
-      console.log(image)
-      return (
-        <div className="col-12"  key={i} >
-        <img  src={image} width="64" height="64" />
-        </div>
-      )
-    })
-    return (
-      <div className="grd-row">
-      {examples}
-      </div>
-    )
+      console.log(image);
+      return React.createElement(
+        'div',
+        { className: 'col-12', key: i },
+        React.createElement('img', { src: image, width: '64', height: '64' })
+      );
+    });
+    return React.createElement(
+      'div',
+      { className: 'grd-row' },
+      examples
+    );
   }
-})
+});
 
-ReactDOM.render(<FetchImg/>,document.getElementById('root'));
+React.render(React.createElement(FetchImg, null), document.getElementById('root'));

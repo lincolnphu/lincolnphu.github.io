55736c4f7cea3e9ef9f63599d26db933cf1d3074
lincolnphu <fjfoursg@gmail.com>
2016-04-28 19:54:59 +0800

    Initial commit

---
+++ d3/Path1.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import d3 from 'd3';
+
+export default class Path1 extends Component {
+  render(){
+    console.log(this.props)
+    return (
+      <path></path>
+    )
+  }
+}

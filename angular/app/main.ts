867ff6b0c01da1b837b6d2c3e73926059bdc7999
lincolnphu <fjfoursg@gmail.com>
2016-05-14 15:56:27 +0800

    Initial commit

---
+++ angular/app/main.ts
@@ -0,0 +1,9 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+import { filterChart } from './app.component';
+import {enableProdMode} from '@angular/core';
+
+enableProdMode()
+
+
+bootstrap(filterChart);
